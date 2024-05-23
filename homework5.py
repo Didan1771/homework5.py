f7ba6cc389ffbb433e9dc9f95297fda64e540efe
Didan1771 <didan1771@icloud.com>
2024-05-23 23:47:03 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+
+immutable_var=1,2,'abc'
+print(immutable_var)
+#элементы кортежа нельзя изменить, так как это неизменяемый объект.
+mutable_list=[3, 'ciko', 7]
+print(mutable_list)
+mutable_list[2]='coconut'
+print(mutable_list)
